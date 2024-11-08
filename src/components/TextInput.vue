d0dda84bf008dcb573e4fd935db911a67d4847f9
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2024-11-08 17:34:15 +0000

    Removed unexplainable oddity with label placement in form inputs.

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,50 +1,32 @@
 <template>
-  <div class="flex flex-col-reverse gap-1">
+  <div class="flex flex-col gap-1">
+
+    <label :for="uid" :class="[
+      'font-semibold flex flex-row gap-1 items-center',
+      shouldDisplayValidationState
+        ? 'peer-invalid:text-red-800 peer-invalid:dark:text-red-200'
+        : '',
+      level === 0 ? 'text-lg' : '',
+    ]">
+      <span>{{ label }}</span>
+      <span v-if="required" class="sr-only">({{ strings.required_field_label }})</span>
+      <span v-if="required" aria-hidden="true" class="text-red-800 dark:text-red-200">*</span>
+    </label>
+
+    <component :is="type === 'multiline' ? 'textarea' : 'input'" v-bind="$attrs" :type="type" :class="[
+      ...baseClasses,
+      type === 'multiline' ? 'h-64' : '',
+      shouldDisplayValidationState
+        ? 'invalid:bg-red-100 invalid:dark:bg-red-900'
+        : '',
+    ]" :id="uid" :aria-labelledby="hint ? `hint_${uid}` : null" :value="modelValue" :required="required"
+      :aria-required="required" @input="$emit('update:modelValue', $event.target.value)"
+      @invalid="shouldDisplayValidationState = true" />
+
+
     <div v-if="hint" :id="`hint_${uid}`" class="text-sm">
       {{ hint }}
     </div>
-
-    <component
-      :is="type === 'multiline' ? 'textarea' : 'input'"
-      v-bind="$attrs"
-      :type="type"
-      :class="[
-        ...baseClasses,
-        type === 'multiline' ? 'h-64' : '',
-        shouldDisplayValidationState
-          ? 'invalid:bg-red-100 invalid:dark:bg-red-900'
-          : '',
-      ]"
-      :id="uid"
-      :aria-labelledby="hint ? `hint_${uid}` : null"
-      :value="modelValue"
-      :required="required"
-      :aria-required="required"
-      @input="$emit('update:modelValue', $event.target.value)"
-      @invalid="shouldDisplayValidationState = true"
-    />
-
-    <label
-      :for="uid"
-      :class="[
-        'font-semibold flex flex-row gap-1 items-center',
-        shouldDisplayValidationState
-          ? 'peer-invalid:text-red-800 peer-invalid:dark:text-red-200'
-          : '',
-        level === 0 ? 'text-lg' : '',
-      ]"
-    >
-      <span>{{ label }}</span>
-      <span v-if="required" class="sr-only"
-        >({{ strings.required_field_label }})</span
-      >
-      <span
-        v-if="required"
-        aria-hidden="true"
-        class="text-red-800 dark:text-red-200"
-        >*</span
-      >
-    </label>
   </div>
 </template>
 
