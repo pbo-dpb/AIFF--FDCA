c6415d25b2009012b36137f48653f710013017f9
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2024-11-08 14:56:06 +0000

    Redesigned Pinia mounting mechanism to reflect how it's done in official Vue doc.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,14 @@
 import { defineCustomElement } from "vue";
 import { createPinia } from "pinia";
-import { createApp } from "vue";
 import App from "./App.ce.vue";
 
-const createCustomElement = () => {
-
-  const app = createApp({});
-  const pinia = createPinia();
-  app.use(pinia);
-
-  const CustomElement = defineCustomElement(App);
-
-  const originalConnectedCallback = CustomElement.prototype.connectedCallback;
-  CustomElement.prototype.connectedCallback = function () {
-
-    const app = createApp({});
-    const pinia = createPinia();
-    app.use(pinia);
-
-    this._pinia = pinia;
-
-    originalConnectedCallback.call(this);
-  };
-
-  return CustomElement;
-};
 
 // Define the custom element
-customElements.define("pbotool-aiff-fdca", createCustomElement());
+customElements.define("pbotool-aiff-fdca", defineCustomElement(App, {
+  configureApp(app) {
+    const pinia = createPinia();
+    app.use(pinia);
+  }
+}));
+
+
